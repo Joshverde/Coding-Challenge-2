f7fb63713a23abccac77a7bc89fb898f22b28727
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-01-26 18:11:06 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -2,3 +2,8 @@ let products = new Array ("Civc", "Accord", "CR-V", "Ridgeline", "Pilot");
 products.push("HR-V");
 products.pop("HR-V")
 console.log(products);
+
+let scores = [1,3,5,7,9];
+scores[1]  = 100;
+let average =(scores[0] + scores[1]+ scores[2]+ scores[3]+ scores[4]) / scores.length;
+console.log(average)
