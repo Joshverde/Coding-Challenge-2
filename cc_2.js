ebb577a4a43f144f611def1ecfdbdc871da18e57
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-01-26 18:12:37 -0500

    Task 3 - Employee Records

--- cc_2.js
+++ cc_2.js
@@ -7,3 +7,13 @@ let scores = [1,3,5,7,9];
 scores[1]  = 100;
 let average =(scores[0] + scores[1]+ scores[2]+ scores[3]+ scores[4]) / scores.length;
 console.log(average)
+
+let employee = {
+    name: "Jane",
+    age: 25,
+    department: "HR",
+    isActive: true
+};
+employee.department = "Sales";
+employee.postion =  "External Sales Manager";
+console.log(employee);
